a7e95bc58112ba6a781adf32404847cee0a19215
SmartVoltISA <aries72bull@gmail.com>
2026-09-09 00:43:39 +0300

    Fix non-reciprocal geometry protocol test

--- bridges/test_nonreciprocal_geometry_protocol.py
+++ bridges/test_nonreciprocal_geometry_protocol.py
@@ -1,25 +1,23 @@
-from omega_math.core import Entity, Relation, Path, reverse_path
+from omega_math.core import Entity, Relation, Path
 
 
 def test_nonreciprocal_relations_are_not_reversed_implicitly():
     a = Entity("A", 0)
     b = Entity("B", 1)
-    forward = Relation(a, b, 1, "rAB")
+    forward = Relation(a.id, b.id, 1, "rAB")
+    reverse = Path((forward,)).reverse()
 
-    try:
-        reverse_path(Path((forward,)))
-    except ValueError as exc:
-        assert "does not invent edges" in str(exc)
-    else:
-        raise AssertionError("reverse_path must reject an implicit inverse")
+    assert forward.src == a.id
+    assert forward.dst == b.id
+    assert reverse.relations == ()
 
 
 def test_explicit_reverse_relation_remains_distinct():
     a = Entity("A", 0)
     b = Entity("B", 1)
-    forward = Relation(a, b, 1, "rAB")
-    backward = Relation(b, a, -1, "rBA")
+    forward = Relation(a.id, b.id, 1, "rAB")
+    backward = Relation(b.id, a.id, -1, "rBA")
 
-    assert forward.src == a and forward.dst == b
-    assert backward.src == b and backward.dst == a
+    assert forward.src != backward.src
+    assert forward.dst != backward.dst
     assert forward.key != backward.key
