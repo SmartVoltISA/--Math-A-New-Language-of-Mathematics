107cfb69dbca57972b609ce98dcaf6bb19d12b12
SmartVoltISA <aries72bull@gmail.com>
2026-09-09 11:06:53 +0300

    Expose reciprocity defect invariant

--- omega_math/__init__.py
+++ omega_math/__init__.py
@@ -2,8 +2,9 @@
 from .core import Entity, Relation, Configuration, State, Path, Transformation, Observation
 from .runtime import execute, execute_ir
 from .research import ResearchRecord, SCHEMA_VERSION
+from .invariants import reciprocity_defect
 
 __all__ = [
     "Entity", "Relation", "Configuration", "State", "Path", "Transformation", "Observation",
-    "execute", "execute_ir", "ResearchRecord", "SCHEMA_VERSION",
+    "execute", "execute_ir", "ResearchRecord", "SCHEMA_VERSION", "reciprocity_defect",
 ]
